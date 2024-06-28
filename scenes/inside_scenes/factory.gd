83c6ba7899406b45f30973b7555ccc491f9989f7
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2024-06-28 14:16:48 -0500

    finished factory

--- scenes/inside_scenes/factory.gd
+++ scenes/inside_scenes/factory.gd
@@ -1,26 +1,23 @@
 extends Node2D
 
-
 var backgrounds: Array[Texture2D] = [
-	load("res://Assets/ALL OTHER ASSETS/nature dead.png"), 
-	load("res://Assets/ALL OTHER ASSETS/nature greener.png"),
-	load("res://Assets/ALL OTHER ASSETS/nature even greener.png"),
-	load("res://Assets/ALL OTHER ASSETS/nature greenest.png")
+	load("res://New Assets/Nature/dead_nature.png"), 
+	load("res://New Assets/Nature/semi_dead_nature.png"),
+	load("res://New Assets/Nature/nature.png"),
+	load("res://New Assets/Nature/good_nature.png")
 ]
 @onready var current_background: Sprite2D = $"Backgrounds/Main Background"
-
 
 func _ready() -> void:
 	Globals.connect("action_completed", change_environment)
 
-
 func change_environment() -> void:
 	var current_environment_indicators: float = Globals.stats["environment_indicators"]
-	if current_environment_indicators < 0:
+	if current_environment_indicators < 25:
 		current_background.texture = backgrounds[0]
-	elif current_environment_indicators < 0.5:
+	elif current_environment_indicators < 50:
 		current_background.texture = backgrounds[1]
-	elif current_environment_indicators < 1:
+	elif current_environment_indicators < 75:
 		current_background.texture = backgrounds[2]
 	else:
 		current_background.texture = backgrounds[3]
